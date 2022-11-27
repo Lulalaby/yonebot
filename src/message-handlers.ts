0d896d4dcb433ac1cbf202266a1625a0c8836a30
Zack <83469289+zzackey@users.noreply.github.com>
2022-11-27 22:56:56 +0530

    fixed ig finally, never used JS

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -4,8 +4,8 @@ export async function handleMessage(client: Client, message: Message) {
   if (message.author.id === client.user?.id) {
     return
   }
-  const yoni = ['yoni','uni','yone']
-  if(message.content.toLowerCase().includes(yoni)) {
+
+  if ((message.content.toLowerCase().includes('yoni')) or (message.content.toLowerCase().includes('uni')) or (message.content.toLowerCase().includes('yone')) {
     message.channel.send('yone')
     try {
       await message.react('940082584896028762')
