bfd9b9e06e0bf8e4d6c9d8d8d7c5c2dfd2e04a10
Lala Sabathil <aiko@aitsys.dev>
2022-12-23 02:09:15 +0100

    React on daddy as well

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -1,6 +1,6 @@
 import {Client, Message} from 'discord.js'
 
-const wordsToReactTo = [/\byoni/i, /\buni/i, /\byone/i]
+const wordsToReactTo = [/\byoni/i, /\buni/i, /\byone/i, /\bdaddy/i]
 
 export async function handleMessage(client: Client, message: Message) {
     if (message.author.id === client.user?.id) {
